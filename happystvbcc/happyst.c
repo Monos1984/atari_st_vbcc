80113cc841e408d4a12e77cdd23af371bae0d93f
Loïc <monos-oniromancie@live.fr>
2021-05-23 19:05:11 +0200

    Add files via upload

--- happystvbcc/happyst.c
+++ happystvbcc/happyst.c
@@ -24,6 +24,11 @@ int   Save_Ecran_Mode_Video;
 short Save_Buffer_Palette[16];
 
 
+short get_test()
+{
+  return 10;
+}
+
 
 /*
   2 octets / 2 octets / 2 octets
@@ -152,26 +157,52 @@ void bip_clavier_on()
 }
 
 // ===========================================================
-// * Fonction ....: load_picture(char* name)                 *
+// * Fonction ....: load_picture(...)                        *
 // ===========================================================
 //  Role .........: Charge et affiche une image              * 
 //  Parametre ....: char* name                               *
 //  ..............:   Adresse de l'image                     *
+//  ..............: long* destination                        *
+//  ..............:   Adresse cible des datas                *
+//  ..............: unsigned char option                     *
+//  ..............:   Option de la fonction                  *
 //  Retour .......: Aucun                                    *
 // ===========================================================
-void load_picture(char* name,long* destination)
+void load_picture(char* name,long* destination,unsigned char option)
 {
   // Offset 
   // 0     = Resolution d'ecran xx yy xx (compresser ou pas) 
   // 2     = Palette de couleur (32 octets)
   //34     = Data Image (32ko)
   
+  // ----------
+  // * Option *
+  // ----------
+  /*
+    xxxxxabc
+    
+    a = 1 : active la résolution
+    b = 1 : active la palette de couleur
+    c = 1 : active la copie de data
+  
+    Compression d'image :
+    pseudo RLE :
+    Ligne par ligne
+    
+    128*
+    
+    
+  
+  
+  */
+    
   // -----------------------------
   // * Declaration des variables *
   // -----------------------------
   signed int f_handles;
   unsigned char picture_buffer[32034];
-
+ /* char *adr_palette;
+ adr_palette = &picture_buffer[2];*/
   // -------------------------
   // * Chargement de l'image *
   // -------------------------  
@@ -193,14 +224,36 @@ void load_picture(char* name,long* destination)
     Fclose(f_handles);
 
     // ------------------------------------------
+    // * Configuration de la résolution d'écran *
+    // ------------------------------------------
+    // * 1er octet = h0, image non compresser, et si il est égale à h80 image compresser.
+    // * 2em octets de l'image 0,1,2 pour la résolution de l'image.
+
+    if ((option&0x4)==0x4) //  %100
+    {
+      Setscreen(Logbase(),Physbase(),picture_buffer[1]);
+    }
+    
+    // ------------------------------------------
     // * Configuration de la palette de couleur *
     // ------------------------------------------
-    Setpalette(picture_buffer+2);
+   
+    if ((option&0x2)==0x2)
+    {
+     // Setpalette((picture_buffer)+2);
+       // Setpalette((void*)adr_palette);
+     Setpalette((void*)&picture_buffer[2]);
+   }
 
-    // ---------------------------------------
-    // * Afficher l'image à l'écran physique *
-    // ---------------------------------------
-    memcpy((char*)destination ,(picture_buffer)+34,32000);
+    // -----------------------------------
+    // * Afficher l'image dans le buffer *
+    // -----------------------------------
+    
+    if ((option&0x1)==0x1)
+    {
+      memcpy((char*)destination ,(picture_buffer)+34,32000);
+    }
+ 
   }
   
   // ------------------------
